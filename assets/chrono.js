fa0791296602c606605a26f14f974367ea43b5d2
Tyler Yates <tyler.yates@utexas.edu>
2017-03-04 21:35:30 -0600

    Make Chronogram object so that multiple things can be returned

--- assets/chrono.js
+++ assets/chrono.js
@@ -1,8 +1,6 @@
-/**
- * @return {number}
- */
-function CalculateChronogramDate(text) {
+function Chronogram(text) {
     var date = 0;
+    var used_characters = [];
     for (var i = 0, len = text.length; i < len; i++) {
         var character = text[i];
         var upperCaseRegex = /[A-Z]/;
@@ -10,28 +8,36 @@ function CalculateChronogramDate(text) {
             switch (character) {
                 case 'I':
                     date += 1;
+                    used_characters.push(character);
                     break;
                 case 'V':
                     date += 5;
+                    used_characters.push(character);
                     break;
                 case 'U':
                     console.log("Upper-case letter 'U' encountered. Assuming value of 5.");
                     date += 5;
+                    used_characters.push(character);
                     break;
                 case 'X':
                     date += 10;
+                    used_characters.push(character);
                     break;
                 case 'L':
                     date += 50;
+                    used_characters.push(character);
                     break;
                 case 'C':
                     date += 100;
+                    used_characters.push(character);
                     break;
                 case 'D':
                     date += 500;
+                    used_characters.push(character);
                     break;
                 case 'M':
                     date += 1000;
+                    used_characters.push(character);
                     break;
                 default:
                     console.info("An upper-case character was encountered that is not a valid Roman Numeral: " + character);
@@ -39,5 +45,6 @@ function CalculateChronogramDate(text) {
         }
     }
 
-    return date;
+    this.date = date;
+    this.used_characters = used_characters;
 }
