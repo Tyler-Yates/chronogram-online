9a106ade7fed8f27a7383997923c8c05428b5bee
Tyler Yates <tyler.yates@utexas.edu>
2017-03-04 22:58:00 -0600

    Use a map for roman numeral values.

--- assets/chrono.js
+++ assets/chrono.js
@@ -1,52 +1,23 @@
+values = {
+    'I': 1,
+    'J': 1,
+    'V': 5,
+    'U': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+};
+
 function Chronogram(text) {
     var date = 0;
     var used_characters = [];
     for (var i = 0, len = text.length; i < len; i++) {
         var character = text[i];
-        var upperCaseRegex = /[A-Z]/;
-        if (upperCaseRegex.test(character)) {
-            switch (character) {
-                case 'I':
-                    date += 1;
-                    used_characters.push(character);
-                    break;
-                case 'J':
-                    console.log("Upper-case letter 'J' encountered. Assuming value of 1.");
-                    date += 1;
-                    used_characters.push(character);
-                    break;
-                case 'V':
-                    date += 5;
-                    used_characters.push(character);
-                    break;
-                case 'U':
-                    console.log("Upper-case letter 'U' encountered. Assuming value of 5.");
-                    date += 5;
-                    used_characters.push(character);
-                    break;
-                case 'X':
-                    date += 10;
-                    used_characters.push(character);
-                    break;
-                case 'L':
-                    date += 50;
-                    used_characters.push(character);
-                    break;
-                case 'C':
-                    date += 100;
-                    used_characters.push(character);
-                    break;
-                case 'D':
-                    date += 500;
-                    used_characters.push(character);
-                    break;
-                case 'M':
-                    date += 1000;
-                    used_characters.push(character);
-                    break;
-                default:
-                    console.info("An upper-case character was encountered that is not a valid Roman Numeral: " + character);
-            }
+        if (character in values) {
+            date += values[character];
+            used_characters.push(character);
         }
     }
 
@@ -62,15 +33,11 @@ function FormattedText(text) {
     var result = "";
     for (var i = 0, len = text.length; i < len; i++) {
         var character = text[i];
-        var upperCaseRegex = /[A-Z]/;
-        var lowerCaseRegex = /[a-z]/;
 
-        if (lowerCaseRegex.test(character)) {
-            result += "<span class='lower'>" + character.toUpperCase() + "</span>";
-        } else if (upperCaseRegex.test(character)) {
+        if (character in values) {
             result += "<span class='upper'>" + character + "</span>";
         } else {
-            result += character;
+            result += "<span class='lower'>" + character.toUpperCase() + "</span>";
         }
     }
 
